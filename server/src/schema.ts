83e5bbf441d7b1f43b28b0031cd3ab47b0ae519d
Linda McDevitt <mcdevittcodes@gmail.com>
2024-04-18 18:21:25 -0400

    build schema

---
+++ server/src/schema.ts
@@ -0,0 +1,32 @@
+import gql from "graphql-tag";
+
+export const typeDefs = gql`
+type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+}
+
+"A track is a group of Modules that teaches about a specific topic"
+type Track {
+    id: ID!
+    "track's title"
+    title: String!
+    "track's main author"
+    author: Author!
+    "track's main illustration to display in track card or track page detail"
+    thumbnail: String
+    "track's approximate length to complete, in minutes"
+    length: Int
+    "number of modules this track contains"
+    modulesCount: Int 
+}
+
+"Author of a complete Track or Module"
+type Author {
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture url"
+    photo: String
+}
+`
