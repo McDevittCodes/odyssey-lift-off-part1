27f730ee7ad69c8df9d9f5f90001c825f212b53c
Linda McDevitt <mcdevittcodes@gmail.com>
2024-05-13 17:14:42 -0400

    display grid of tracks and cleanup

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Layout } from "../components";
+import { Layout, QueryResult } from "../components";
 import { gql } from "../__generated__";
-
+import { useQuery } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+ 
 /** TRACKS query to retrieve all tracks */
 const TRACKS = gql(`
   query GetTracks {
@@ -21,11 +23,21 @@ const TRACKS = gql(`
 `);
 
 /**
- * Tracks Page is the Catstronauts home page.
- * We display a grid of tracks fetched with useQuery with the TRACKS query
- */
+* Tracks Page is the Catstronauts home page.
+* We display a grid of tracks fetched with useQuery with the TRACKS query
+*/
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+   
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
-
+ 
 export default Tracks;
